2452c88deaa6e3bb13a4b30fb76e50e4c822f882
An ByeongUk <quddnr005@naver.com>
2021-09-23 14:32:02 +0900

    event-bubbling

--- js/tab.js
+++ js/tab.js
@@ -1,37 +1,49 @@
-$(".tab-button")
-  .eq(0)
-  .click(function () {
-    $(".tab-button").removeClass("active");
-    $(".tab-content").removeClass("show");
-    $(".tab-button").eq(0).addClass("active");
-    $(".tab-content").eq(0).addClass("show");
-  });
+// $(".tab-button")
+//   .eq(0)
+//   .click(function () {
+//     $(".tab-button").removeClass("active");
+//     $(".tab-content").removeClass("show");
+//     $(".tab-button").eq(0).addClass("active");
+//     $(".tab-content").eq(0).addClass("show");
+//   });
 
-$(".tab-button")
-  .eq(1)
-  .click(function () {
-    $(".tab-button").removeClass("active");
-    $(".tab-content").removeClass("show");
-    $(".tab-button").eq(1).addClass("active");
-    $(".tab-content").eq(1).addClass("show");
-  });
+// $(".tab-button")
+//   .eq(1)
+//   .click(function () {
+//     $(".tab-button").removeClass("active");
+//     $(".tab-content").removeClass("show");
+//     $(".tab-button").eq(1).addClass("active");
+//     $(".tab-content").eq(1).addClass("show");
+//   });
 
-$(".tab-button")
-  .eq(2)
-  .click(function () {
-    $(".tab-button").removeClass("active");
-    $(".tab-content").removeClass("show");
-    $(".tab-button").eq(2).addClass("active");
-    $(".tab-content").eq(2).addClass("show");
-  });
+// $(".tab-button")
+//   .eq(2)
+//   .click(function () {
+//     $(".tab-button").removeClass("active");
+//     $(".tab-content").removeClass("show");
+//     $(".tab-button").eq(2).addClass("active");
+//     $(".tab-content").eq(2).addClass("show");
+//   });
 
-for (let i = 0; i < 2; i++) {
-  $(".tab-button")
-    .eq(i)
-    .click(function () {
-      $(".tab-button").removeClass("active");
-      $(".tab-content").removeClass("show");
-      $(".tab-button").eq(i).addClass("active");
-      $(".tab-content").eq(i).addClass("show");
-    });
+// for (let i = 0; i < 2; i++) {
+//   $(".tab-button")
+//     .eq(i)
+//     .click(function () {
+//       $(".tab-button").removeClass("active");
+//       $(".tab-content").removeClass("show");
+//       $(".tab-button").eq(i).addClass("active");
+//       $(".tab-content").eq(i).addClass("show");
+//     });
+// }
+
+function tabopen(i) {
+  $(".tab-button").removeClass("active");
+  $(".tab-content").removeClass("show");
+  $(".tab-button").eq(i).addClass("active");
+  $(".tab-content").eq(i).addClass("show");
 }
+
+$(".list").click(function (e) {
+  // console.log(e.target.dataset.id);
+  tabopen(e.target.dataset.id);
+});
